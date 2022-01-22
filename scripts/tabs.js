8592e0f441a45e27fca706a86d19165d3b1ced22
rus733 <9282716617@mail.ru>
2022-01-22 14:06:35 +0300

    add change textContent title window  in  tabs.js

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -4,42 +4,47 @@ const tabsFunc = () => {
   const tabs = document.querySelectorAll('.card-detail__change');
   const tabsTitle = document.querySelector('.card-details__title');
   const tabsPrice = document.querySelector('.card-details__price');
-  const tabsImage = document.querySelector('.card__image_item'); //title>
+  const tabsImage = document.querySelector('.card__image_item');
+  const title = document.querySelector('title');
 
   const tabsOptions = [
     {
-      // 01
       name: 'Graphite',
       memory: '128',
       price: '60000',
       image: 'img/iPhone-graphite.webp',
+      title: ' 128GB Graphite',
     },
 
     {
-      // 02
       name: 'Silver',
       memory: '256',
       price: '65000',
       image: 'img/iPhone-silver.webp',
+      title: ' 256GB Silver',
     },
 
     {
-      //03
       name: 'Sierra Blue',
       memory: '512',
       price: '70000',
-      image: 'img/iPhone-sierra_blue.webp', //iPhone-sierra_blue.webp
+      image: 'img/iPhone-sierra_blue.webp',
+      title: ' 512GB Sierra Blue',
     },
   ];
   //ф. изменения содержание заголовка и цены таба
   const changeContent = (index) => {
+    //изменение текста емкость памяти  и название смартфона
     tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
 
-    tabsPrice.textContent = `${tabsOptions[index].price}₽`;
+    tabsPrice.textContent = `${tabsOptions[index].price}₽`; //изменение цены
 
-    tabsImage.setAttribute('src', tabsOptions[index].image);
-    //tabsImage.src = `${tabsOptions[index].image}`;
+    tabsImage.setAttribute('src', tabsOptions[index].image); // изменение картинки
+    //tabsImage.src = `${tabsOptions[index].image}`;// чем этот вариант хуже ?
+
+    title.textContent = `iPhone 13 Pro ${tabsOptions[index].title}`;
   };
+
   //ф. выбора активного таба
   const changeActiveTabs = (indexClickedTab) => {
     tabs.forEach((tab, index) => {
@@ -51,6 +56,7 @@ const tabsFunc = () => {
 
     changeContent(indexClickedTab);
   };
+
   //ф. перебора всех ссылок табов и прослушивателя события клик
   tabs.forEach((tab, index) => {
     tab.addEventListener('click', () => {
