1b1ea7ee0dcb649b4ff2657d04bb94709b85e5f3
rus733 <9282716617@mail.ru>
2022-01-22 13:57:13 +0300

    run change data in tabs, incapsulate tabs.js scroll.js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,42 +1,33 @@
-const links = document.querySelectorAll('.header-menu__item a');
-const linkFullChar = document.querySelector('.card-details__link-characteristics');
-const arr = [...links, linkFullChar];
+const scrollFunc = () => {
+  const links = document.querySelectorAll('.header-menu__item a');
+  const linkFullChar = document.querySelector('.card-details__link-characteristics');
+  const arr = [...links, linkFullChar];
 
-seamless.polyfill(); // полифил
+  seamless.polyfill(); // полифил
 
-arr.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault();
-    const id = element.getAttribute('href').substring(1);
+  arr.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+      const id = element.getAttribute('href').substring(1);
 
-    const section = document.getElementById(id);
+      const section = document.getElementById(id);
 
-    if (section) {
-      // полифилы дают использовать кросбраузерные методы
-      // вот этот вариант работанет в сафари
-      seamless.elementScrollIntoView(section, {
-        behavior: 'smooth',
-        block: 'start',
-      });
-    } else {
-      seamless.elementScrollIntoView(document.querySelector('#characteristics'), {
-        behavior: 'smooth',
-        block: 'center',
-        inline: 'center',
-      });
-    }
+      if (section) {
+        // полифилы дают использовать кросбраузерные методы
+        // вот этот вариант работанет в сафари
+        seamless.elementScrollIntoView(section, {
+          behavior: 'smooth',
+          block: 'start',
+        });
+      } else {
+        seamless.elementScrollIntoView(document.querySelector('#characteristics'), {
+          behavior: 'smooth',
+          block: 'center',
+          inline: 'center',
+        });
+      }
+    });
   });
-});
-/*
-link2.addEventListener('click', (event) => {
-  event.preventDefault();
-  const id = link2.getAttribute('href').substring(1);
-  console.log(id);
-  const section = document.getElementById(id);
-  console.log(link2);
-  seamless.elementScrollIntoView(section, {
-    behavior: 'smooth',
-    block: 'start',
-  });
-});
-*/
+};
+
+scrollFunc();
--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,2 +1,64 @@
 'use strict';
-console.log('tabs');
+
+const tabsFunc = () => {
+  const tabs = document.querySelectorAll('.card-detail__change');
+  const tabsTitle = document.querySelector('.card-details__title');
+  const tabsPrice = document.querySelector('.card-details__price');
+  const tabsImage = document.querySelector('.card__image_item'); //title>
+
+  const tabsOptions = [
+    {
+      // 01
+      name: 'Graphite',
+      memory: '128',
+      price: '60000',
+      image: 'img/iPhone-graphite.webp',
+    },
+
+    {
+      // 02
+      name: 'Silver',
+      memory: '256',
+      price: '65000',
+      image: 'img/iPhone-silver.webp',
+    },
+
+    {
+      //03
+      name: 'Sierra Blue',
+      memory: '512',
+      price: '70000',
+      image: 'img/iPhone-sierra_blue.webp', //iPhone-sierra_blue.webp
+    },
+  ];
+  //ф. изменения содержание заголовка и цены таба
+  const changeContent = (index) => {
+    tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
+
+    tabsPrice.textContent = `${tabsOptions[index].price}₽`;
+
+    tabsImage.setAttribute('src', tabsOptions[index].image);
+    //tabsImage.src = `${tabsOptions[index].image}`;
+  };
+  //ф. выбора активного таба
+  const changeActiveTabs = (indexClickedTab) => {
+    tabs.forEach((tab, index) => {
+      tab.classList.remove('active');
+      if (index === indexClickedTab) {
+        tab.classList.add('active');
+      }
+    });
+
+    changeContent(indexClickedTab);
+  };
+  //ф. перебора всех ссылок табов и прослушивателя события клик
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      changeActiveTabs(index);
+    });
+  });
+
+  changeContent(0); // первый вызов для
+};
+
+tabsFunc();
