a34593a5f19386cf5c2ec0bc2c320b1fba593646
rus733 <9282716617@mail.ru>
2022-01-23 17:27:39 +0300

    task3: del  close first tab

--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -1,22 +1,15 @@
 const accordeon = () => {
   const chItems = document.querySelectorAll('.characteristics__item');
-  // открыты й первый  блок
-  /*
-  const chItem = document.querySelector('.characteristics__item');
 
-  chItem.querySelector('.characteristics__title').classList.add('active');
-  chItem.querySelector('.characteristics__description').classList.add('open');
-  chItem.querySelector('.characteristics__description').style.height = chItem.querySelector('.characteristics__description').scrollHeight + 'px';
-*/
-  //
   chItems.forEach((item) => {
     const chButton = item.querySelector('.characteristics__title');
     const chContent = item.querySelector('.characteristics__description');
 
-    chButton.addEventListener('click', (e) => {
+    chButton.addEventListener('click', () => {
       chItems.forEach((item) => {
         const chButton = item.querySelector('.characteristics__title');
         const chContent = item.querySelector('.characteristics__description');
+
         chContent.style.height = '';
         chButton.classList.remove('active');
         chContent.classList.remove('open');
@@ -25,7 +18,6 @@ const accordeon = () => {
       if (chContent.classList.contains('open')) {
         chContent.style.height = '';
       } else {
-        //console.log(chContent.scrollHeight);//высота раскрываемого блока
         chContent.style.height = chContent.scrollHeight + 'px';
       }
       chButton.classList.toggle('active');
