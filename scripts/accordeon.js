1b0f160817cb39b22232e376daf60cd6391d7258
rus733 <9282716617@mail.ru>
2022-01-23 17:43:50 +0300

    task3: del comment code

--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -5,7 +5,6 @@ const accordeon = () => {
     chItems.forEach((item) => {
       item.querySelector('.characteristics__title').classList.remove('active');
       item.querySelector('.characteristics__description').classList.remove('open');
-      //el.querySelector('.characteristics__description').classList.remove('active');
       item.querySelector('.characteristics__description').style.height = '';
     });
   };
@@ -16,15 +15,6 @@ const accordeon = () => {
     const chContent = item.querySelector('.characteristics__description');
 
     chButton.addEventListener('click', () => {
-      // chItems.forEach((item) => {
-      //   const chButton = item.querySelector('.characteristics__title');
-      //   const chContent = item.querySelector('.characteristics__description');
-
-      //   chContent.style.height = '';
-      //   chButton.classList.remove('active');
-      //   chContent.classList.remove('open');
-      // });
-
       if (chContent.classList.contains('open')) {
         chContent.style.height = '';
       } else {
