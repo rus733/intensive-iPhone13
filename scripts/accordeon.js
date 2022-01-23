c8eaf31029cc84b4b8a95b2813815f060e22102a
rus733 <9282716617@mail.ru>
2022-01-23 17:07:50 +0300

    task3: add open first tab accordeon

--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -1,13 +1,27 @@
 const accordeon = () => {
   const chItems = document.querySelectorAll('.characteristics__item');
+  // открыты й первый  блок
 
-  console.log(); //characteristics__description active
+  const chItem = document.querySelector('.characteristics__item');
+
+  chItem.querySelector('.characteristics__title').classList.add('active');
+  chItem.querySelector('.characteristics__description').classList.add('open');
+  chItem.querySelector('.characteristics__description').style.height = chItem.querySelector('.characteristics__description').scrollHeight + 'px';
+
+  //
   chItems.forEach((item) => {
     const chButton = item.querySelector('.characteristics__title');
     const chContent = item.querySelector('.characteristics__description');
-    //console.log(chContent);
 
-    chButton.addEventListener('click', () => {
+    chButton.addEventListener('click', (e) => {
+      chItems.forEach((item) => {
+        const chButton = item.querySelector('.characteristics__title');
+        const chContent = item.querySelector('.characteristics__description');
+        chContent.style.height = '';
+        chButton.classList.remove('active');
+        chContent.classList.remove('open');
+      });
+
       if (chContent.classList.contains('open')) {
         chContent.style.height = '';
       } else {
